ad0b78afbb404322da21dc6cda8e5dfe6beba9ab
srav-athina <jna.athina@gmail.com>
2023-04-20 21:30:22 -0400

    more changes

---
+++ Honors Thesis/scheduler/LocalThesis/algorithm/population.py
@@ -0,0 +1,24 @@
+from schedule import Schedule
+
+class Population:
+    def __init__(self, size, courses, dependencies, profs, hard_prefs, soft_prefs, course_prefs, rooms):
+        self.size = size
+        self.schedules = []
+        for i in range(size):
+            self.schedules.append(Schedule(courses, dependencies, profs, hard_prefs, soft_prefs, course_prefs, rooms))
+        self.viable_schedules = []
+
+    def get_schedules(self):
+        return self.schedules
+
+    def get_viable_schedules(self):
+        for schedule in self.schedules:
+            if schedule.check_validity():
+                self.viable_schedules.append(schedule)
+        return self.viable_schedules
+
+    def __iter__(self):
+        return iter(self.schedules)
+
+    def __len__(self):
+        return len(self.schedules)
